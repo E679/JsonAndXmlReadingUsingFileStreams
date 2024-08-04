2f45bcda61086abd47f6eecbcda44353d2c29303
Supreeth Goud <saisupreethgoud4@gmail.com>
2024-08-04 17:58:32 +0530

    JsonAndXMLReading using buferred reader

--- src/main/java/CreateDTOAndReadUsingFileStreams/createDTOAndReadJsonFile.java
+++ src/main/java/CreateDTOAndReadUsingFileStreams/createDTOAndReadJsonFile.java
@@ -3,8 +3,11 @@ package CreateDTOAndReadUsingFileStreams;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class createDTOAndReadJsonFile {
     public static void main(String[] args) throws IOException {
@@ -20,6 +23,8 @@ public class createDTOAndReadJsonFile {
 
             inJson = dtoType.getClass().getResourceAsStream(filename);
             dto = objectMapper.readValue(inJson, dtoType.getClass());
+            //Read using buffered reader
+            //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inJson, StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
